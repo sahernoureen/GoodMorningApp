f7aac180198df92b3c2e8f84b4674a2f297a5b28
sahernoureen <58045833+sahernoureen@users.noreply.github.com>
2020-05-17 14:34:35 -0500

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace GoodMorningApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.USD = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Ipadd = new System.Windows.Forms.Label();
+            this.canad = new System.Windows.Forms.Label();
+            this.CountryName = new System.Windows.Forms.Label();
+            this.CAD = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Province = new System.Windows.Forms.Label();
+            this.Curr_Temp = new System.Windows.Forms.Label();
+            this.City = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.BackgroundImage = global::GoodMorningApp.Properties.Resources.sunrise;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.USD);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.Ipadd);
+            this.panel1.Controls.Add(this.canad);
+            this.panel1.Controls.Add(this.CountryName);
+            this.panel1.Controls.Add(this.CAD);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.Province);
+            this.panel1.Controls.Add(this.Curr_Temp);
+            this.panel1.Controls.Add(this.City);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(490, 349);
+            this.panel1.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(401, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 32);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "°C";
+            // 
+            // USD
+            // 
+            this.USD.AutoSize = true;
+            this.USD.BackColor = System.Drawing.Color.Transparent;
+            this.USD.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.USD.Location = new System.Drawing.Point(400, 185);
+            this.USD.Name = "USD";
+            this.USD.Size = new System.Drawing.Size(45, 20);
+            this.USD.TabIndex = 14;
+            this.USD.Text = "USD";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(275, 221);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP add:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(382, 184);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(18, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "$";
+            // 
+            // Ipadd
+            // 
+            this.Ipadd.AutoSize = true;
+            this.Ipadd.BackColor = System.Drawing.Color.Transparent;
+            this.Ipadd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ipadd.Location = new System.Drawing.Point(351, 217);
+            this.Ipadd.Name = "Ipadd";
+            this.Ipadd.Size = new System.Drawing.Size(49, 20);
+            this.Ipadd.TabIndex = 1;
+            this.Ipadd.Text = "Ipadd";
+            // 
+            // canad
+            // 
+            this.canad.AutoSize = true;
+            this.canad.BackColor = System.Drawing.Color.Transparent;
+            this.canad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.canad.Location = new System.Drawing.Point(403, 158);
+            this.canad.Name = "canad";
+            this.canad.Size = new System.Drawing.Size(54, 20);
+            this.canad.TabIndex = 12;
+            this.canad.Text = "canad";
+            // 
+            // CountryName
+            // 
+            this.CountryName.AutoSize = true;
+            this.CountryName.BackColor = System.Drawing.Color.Transparent;
+            this.CountryName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CountryName.Location = new System.Drawing.Point(356, 125);
+            this.CountryName.Name = "CountryName";
+            this.CountryName.Size = new System.Drawing.Size(111, 20);
+            this.CountryName.TabIndex = 3;
+            this.CountryName.Text = "CountryName";
+            // 
+            // CAD
+            // 
+            this.CAD.AutoSize = true;
+            this.CAD.BackColor = System.Drawing.Color.Transparent;
+            this.CAD.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CAD.Location = new System.Drawing.Point(376, 160);
+            this.CAD.Name = "CAD";
+            this.CAD.Size = new System.Drawing.Size(30, 20);
+            this.CAD.TabIndex = 11;
+            this.CAD.Text = "C$";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(241, 158);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(127, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Exchange Rate:";
+            // 
+            // Province
+            // 
+            this.Province.AutoSize = true;
+            this.Province.BackColor = System.Drawing.Color.Transparent;
+            this.Province.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Province.Location = new System.Drawing.Point(271, 124);
+            this.Province.Name = "Province";
+            this.Province.Size = new System.Drawing.Size(78, 20);
+            this.Province.TabIndex = 5;
+            this.Province.Text = "Province,";
+            // 
+            // Curr_Temp
+            // 
+            this.Curr_Temp.AutoSize = true;
+            this.Curr_Temp.BackColor = System.Drawing.Color.Transparent;
+            this.Curr_Temp.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Curr_Temp.Location = new System.Drawing.Point(364, 75);
+            this.Curr_Temp.Name = "Curr_Temp";
+            this.Curr_Temp.Size = new System.Drawing.Size(32, 32);
+            this.Curr_Temp.TabIndex = 9;
+            this.Curr_Temp.Text = "T";
+            // 
+            // City
+            // 
+            this.City.AutoSize = true;
+            this.City.BackColor = System.Drawing.Color.Transparent;
+            this.City.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.City.Location = new System.Drawing.Point(281, 85);
+            this.City.Name = "City";
+            this.City.Size = new System.Drawing.Size(38, 20);
+            this.City.TabIndex = 7;
+            this.City.Text = "City";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 355);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "MorningApp";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label Ipadd;
+        private System.Windows.Forms.Label CountryName;
+        private System.Windows.Forms.Label Province;
+        private System.Windows.Forms.Label City;
+        private System.Windows.Forms.Label Curr_Temp;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label CAD;
+        private System.Windows.Forms.Label canad;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label USD;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoodMorningApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            FindIpaddressInfo();
+            FindTemperatureInfo();
+            FindCurrencyExchangeRate();
+           
+            RegisterInStartup();
+        }
+
+
+        private  void FindIpaddressInfo()
+        {
+            HttpClient httpClient1 = new HttpClient();
+            HttpResponseMessage response1 = httpClient1.GetAsync("https://api.ipdata.co?api-key=18480ad3f417a36564ebff9853dbf2d5d9130d4b1543627fad0d8563").Result;
+            if (response1.IsSuccessStatusCode)
+            {
+                var responseResult1 = response1.Content.ReadAsStringAsync().Result;
+
+                dynamic JsonResult = Newtonsoft.Json.JsonConvert.DeserializeObject(responseResult1);
+                Ipadd.Text = JsonResult.ip;
+                CountryName.Text = JsonResult.country_name;
+                City.Text = JsonResult.city;
+                Province.Text = JsonResult.region;
+            }
+            else
+            {
+                MessageBox.Show("An error Message Occured" + response1.StatusCode + "with reason " + response1.ReasonPhrase);
+            
+            }
+
+        }
+
+
+        public void FindTemperatureInfo()
+        {
+            HttpClient httpClient1 = new HttpClient();
+            HttpResponseMessage response1 = httpClient1.GetAsync("https://api.openweathermap.org/data/2.5/weather?q=Winnipeg,Canada&appid=6a2bc05c86367dc11cb3fef1d07e6cb8&units=metric").Result;
+            if (response1.IsSuccessStatusCode)
+            {
+                var responseResult1 = response1.Content.ReadAsStringAsync().Result;
+            
+                dynamic JsonResult = Newtonsoft.Json.JsonConvert.DeserializeObject(responseResult1);
+                Curr_Temp.Text = JsonResult.main.temp;
+            }
+            else
+            {
+               MessageBox.Show("An error Message Occured" + response1.StatusCode + "with reason " + response1.ReasonPhrase);
+            }
+        }
+
+
+        public void FindCurrencyExchangeRate()
+        {
+            HttpClient httpClient1 = new HttpClient();
+            HttpResponseMessage response1 = httpClient1.GetAsync(" https://api.exchangeratesapi.io/latest?symbols=CAD,USD").Result;
+            if (response1.IsSuccessStatusCode)
+            {
+                var responseResult1 = response1.Content.ReadAsStringAsync().Result;
+
+                dynamic JsonResult = Newtonsoft.Json.JsonConvert.DeserializeObject(responseResult1);
+                canad.Text = JsonResult.rates.CAD;
+                USD.Text = JsonResult.rates.USD;
+            }
+            else
+            {
+                MessageBox.Show("An error Message Occured" + response1.StatusCode + "with reason " + response1.ReasonPhrase);
+            }
+        }
+
+
+        private void RegisterInStartup()
+        {
+            RegistryKey registryKey = Registry.CurrentUser.OpenSubKey
+                    ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+           
+                registryKey.SetValue("GoodMorningApp", Application.ExecutablePath);
+            
+         
+        }
+
+    }
+}
